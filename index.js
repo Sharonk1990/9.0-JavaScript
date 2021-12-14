1e4f83b80223f3de007baed387b84c2f3fb9c966
Sharonk1990 <92334366+Sharonk1990@users.noreply.github.com>
2021-12-14 20:33:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Hello Winc Academy")
+
+let name = 'Sharon';
+console.log(name);
+
+let number = 4+4;
+console.log(number);
+
+let numbers = '4' + '4';
+console.log(numbers);
+
+let addition = 2 + 3;
+console.log(addition);
+
+let subtraction = 10 - 6;
+console.log(subtraction);
+
+let multiplication = 3 * 5;
+console.log(multiplication);
+
+let division = 10 / 2;
+console.log(division);
+
+let modulus = 5%100;
+console.log(modulus);
+
+let age = 31;
+console.log(age);
